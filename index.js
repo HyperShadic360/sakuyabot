a4cf634be314abf15121e903052f1a68bff41fca
Bowen Zhang <bowzotang@gmail.com>
2020-04-05 18:08:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "process.env.PREFIX";
+const token = "process.env.BOT_TOKEN";
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
@@ -37,4 +38,4 @@ client.on('message', msg => {
     channel.send(`Welcome ${member}!`);
   });
 
-client.login(process.env.BOT_TOKEN);
+client.login(token);
