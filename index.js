be23b0308cbfac4e1332b575f961e0540718c857
Bowen Zhang <bowzotang@gmail.com>
2020-07-22 21:07:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.on('message', msg => {
     //const channel = member.guild.channels.cache.find(c => c.name === 'welcome'); // change this to the channel name you want to send the greeting to
     //if (!channel) return;
     //channel.send(`Welcome ${member}! Please enjoy your stay in this degenerate land. Visit the bot-help channel for information on how to use the bots.`);
-    member.guild.channels.get('welcome').send("Welcome ${member}! Please enjoy your stay in this degenerate land. Visit the bot-help channel for information on how to use the bots.");
+    member.guild.channels.get('welcome').send('Welcome ${member}! Please enjoy your stay in this degenerate land. Visit the bot-help channel for information on how to use the bots.');
   });
 
 client.login(token);
